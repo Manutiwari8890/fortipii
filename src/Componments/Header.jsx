6376a1a39a7aecdb98b3abf2a42175823697846b
Manu <tiwarimanu871@gmail.com>
2026-02-08 12:27:41 +0530

    add header animation

--- src/Componments/Header.jsx
+++ src/Componments/Header.jsx
@@ -76,27 +76,27 @@ function Header(){
     return (
         <>
             <header>
-                <nav className={`bg-white min-h-20 content-center py-2 border fixed left-1/2 -translate-x-1/2 z-[999] transition-all duration-300 ease-in-out ${isScroll ? " mx-auto border-[#26365626] rounded-full top-2 shadow-sm sticky-wrapper w-[95%] lg:w-[95%] xl:w-[95%] 2xl:w-3/4" : "w-full border-transparent top-0"} lg:min-h-20 2xl:min-h-25`}>
+                <nav className={`bg-white  border fixed left-1/2 -translate-x-1/2 z-[999] transition-all duration-300 ease-in-out py-4 ${menu ? "min-h-80" : "min-h-20 content-center "} ${isScroll ? " mx-auto border-[#26365626] rounded-full top-2 shadow-sm sticky-wrapper w-[95%] lg:py-2 lg:w-[95%] xl:w-[95%] 2xl:w-3/4" : "w-full border-transparent top-0"} xl:min-h-20 2xl:min-h-25`}>
                     <div className="container mx-auto px-2 lg:px-5">
                         <div className="flex gap-2 items-center justify-between">
                             <Link className="mr-6" to="/">
                                 <img src="/assets/images/fortipii.svg" alt="Fortipii" className="w-45" />
                             </Link>
-                            <ul className={`gap-1 w-max bg-white fixed z-10000 top-0 left-0 relative h-screen overflow-y-auto border-1 border-[#26365626] rounded-full px-2 py-2 block md:z-1000  xl:w-auto xl:flex xl:justify-center xl:relative xl:h-max xl:overflow-y-visible transition-all transition-ease-in-out duration-300 xl:translate-y-0 lg:hidden`}  ref={navRef} onMouseLeave={handleLeave}>
+                            <ul className={`gap-1 fixed z-10000 top-22 left-0 overflow-y-auto border-1 border-transparent rounded-0 px-2 py-2 transition-all transition-ease-in-out duration-300 origin-top ${menu ? "opacity-100 h-50 rotate-x-0 w-full delay-200" : "opacity-0 w-0 relative border-[#26365626] h-0 rotate-x-90" } md:px-8 lg:rounded-full xl:top-0 xl:px-2 xl:w-max xl:border-[#26365626] xl:w-auto xl:flex xl:justify-center xl:relative xl:h-max xl:overflow-y-visible xl:translate-y-0`}  ref={navRef} onMouseLeave={handleLeave}>
                                 <li className="nav-item">
-                                    <NavLink to="/" className={`inline-block text-base text-[#263656] font-medium font-commissioner py-2 px-6 ${hoverTab=="/" ? "text-white" : ""}`} data-page="/" onMouseEnter={handleHover}>Home</NavLink>
+                                    <NavLink to="/" className={`inline-block text-xl text-[#263656] font-medium font-commissioner py-2 px-2 w-full lg:px-6 xl:text-base xl:w-max ${hoverTab=="/" ? "text-primary xl:text-white" : ""}`} data-page="/" onMouseEnter={handleHover}>Home</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="/about" className={`inline-block text-base text-[#263656] font-medium font-commissioner py-2 px-6 ${hoverTab=="/about" ? "text-white" : ""}`}  data-page="/about"  onMouseEnter={handleHover}>About</NavLink>
+                                    <NavLink to="/about" className={`inline-block text-xl text-[#263656] font-medium font-commissioner py-2 px-2 w-full lg:px-6 xl:text-base xl:w-max ${hoverTab=="/about" ? "text-primary xl:text-white" : ""}`}  data-page="/about"  onMouseEnter={handleHover}>About</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="/services" className={`inline-block text-base text-[#263656] font-medium font-commissioner py-2 px-6 ${hoverTab=="/services" ? "text-white" : ""}`}  data-page="/services" onMouseEnter={handleHover}>Service</NavLink>
+                                    <NavLink to="/services" className={`inline-block text-xl text-[#263656] font-medium font-commissioner py-2 px-2 w-full lg:px-6 xl:text-base xl:w-max ${hoverTab=="/services" ? "text-primary xl:text-white" : ""}`}  data-page="/services" onMouseEnter={handleHover}>Service</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="/contact" className={`inline-block text-base text-[#263656] font-medium font-commissioner py-2 px-6 ${hoverTab=="/contact" ? "text-white" : ""}`}  data-page="/contact"  onMouseEnter={handleHover}>Contact</NavLink>
+                                    <NavLink to="/contact" className={`inline-block text-xl text-[#263656] font-medium font-commissioner py-2 px-2 w-full lg:px-6 xl:text-base xl:w-max ${hoverTab=="/contact" ? "text-primary xl:text-white" : ""}`}  data-page="/contact"  onMouseEnter={handleHover}>Contact</NavLink>
                                 </li>
                                 <span
-                                    className="absolute top-1/2 -translate-y-1/2 h-[42px] bg-primary rounded-full transition-all -z-1"
+                                    className="absolute top-1/2 -translate-y-1/2 h-[42px] bg-primary rounded-full transition-all -z-1 hidden xl:block"
                                     style={{
                                         left: `${lineStyle.left}px`,
                                         width: `${lineStyle.width}px`,
@@ -104,13 +104,13 @@ function Header(){
                                 />
                             </ul>
                             <div className="flex gap-2 items-center">
-                                <Link className="btn-primary bg-secondary font-commissioner text-base relative font-medium text-white py-3 px-8 rounded-full">Sign Up</Link>
-                                <Link className="btn-secondary bg-primary font-commissioner text-base relative font-medium text-white py-3 px-8 rounded-full">Login</Link>
-                                <button className={`hidden ml-4 humburger cursor-pointer group text-center content-center lg:block xl:hidden 2xl:hidden ${menu ? "active" : ""}`} onClick={() => setMenu((pre) => !pre)}>
-                                    <div className="flex flex-col overflow-hidden w-9 h-9 justify-center relative gap-1.5 relative m-auto">
-                                        <div className="w-9 h-1 bg-secondary rounded-full line-1"></div>
-                                        <div className="flex justify-between items-center min-h-1.5 line-2">
-                                            <span className="block w-6.5 h-1 bg-secondary rounded-full group-hover:w-8"></span>
+                                <Link className="btn-primary bg-secondary font-commissioner text-base relative font-medium text-white py-3 px-8 rounded-full hidden">Sign Up</Link>
+                                <Link className="btn-secondary bg-primary font-commissioner text-base relative font-medium text-white py-3 px-8 rounded-full hidden">Login</Link>
+                                <button className={`block ml-4 humburger cursor-pointer group text-center content-center lg:block xl:hidden 2xl:hidden ${menu ? "active" : ""}`} onClick={() => setMenu((pre) => !pre)}>
+                                    <div className="flex flex-col overflow-hidden w-12 h-12 justify-center relative gap-1.5 relative m-auto">
+                                        <div className="w-10 h-1 bg-secondary rounded-full line-1"></div>
+                                        <div className="flex justify-start gap-1 items-center min-h-1.5 line-2">
+                                            <span className="block w-7 h-1 bg-secondary rounded-full group-hover:w-8"></span>
                                             <span className="block w-1.5 h-1.5 bg-primary rounded-full group-hover:opacity-0"></span>
                                         </div>
                                         <div className="block w-6 h-1 bg-secondary rounded-full line-3"></div>
